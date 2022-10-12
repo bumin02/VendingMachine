721e4d6c623defdf64c227b8a9521aa1f6933474
Jenny-Linn <Jennylin996@gmail.com>
2022-10-12 21:40:11 +1100

    included login, create account

---
+++ app/src/main/java/SOFT2412_Assignment2_CC_04_Wed_16_Frank_Gorup/Card.java
@@ -0,0 +1,11 @@
+package SOFT2412_Assignment2_CC_04_Wed_16_Frank_Gorup;
+
+public class Card {
+    String number;
+    String name;
+
+    public Card(String number, String name) {
+        this.number = number;
+        this.name = name;
+    }
+}
--- app/src/main/java/SOFT2412_Assignment2_CC_04_Wed_16_Frank_Gorup/vendingMachine.java
+++ app/src/main/java/SOFT2412_Assignment2_CC_04_Wed_16_Frank_Gorup/vendingMachine.java
@@ -5,9 +5,46 @@ import java.util.Locale;
 
 public class vendingMachine {
     ArrayList<Items> items;
+    ArrayList<User> users;
+    User currentUser;
 
     public vendingMachine() {
         this.items = new ArrayList<>();
+        this.users = new ArrayList<>();
+    }
+
+    // return false if account exists or invalid input
+    public boolean createAccount(String username, String password) {
+        if (username == null || password == null) {
+            return false;
+        }
+
+        // check if user exists
+        for (User i : users) {
+            if (i.getAccount().equals(username)) {
+                return false;
+            }
+        }
+        User user = new User(username, password);
+        this.users.add(user);
+        this.currentUser = user;
+        return true;
+    }
+
+    // return false if invalid or user and password combination does not exist
+    public boolean login(String username, String password) {
+        if (username == null || password == null) {
+            return false;
+        }
+
+        // check if user exists
+        for (User i : users) {
+            if (i.getAccount().equals(username) && i.getPassword().equals(password)) {
+                this.currentUser = i;
+                return true;
+            }
+        }
+        return false;
     }
 
     // returns false if no options were found
